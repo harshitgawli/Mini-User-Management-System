f10149e565c8b7146d4f165796b73fef25770788
harshit gawli <harshitgawli@gmail.com>
2025-12-31 09:10:41 +0530

    deploy issue resolve

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mini-user-management-system-d2k8.onrender.com",
+  baseURL: "https://mini-user-management-system-d2k8.onrender.com/api",
 });
 
 api.interceptors.request.use(
